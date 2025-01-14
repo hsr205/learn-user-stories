edc9950dca42e841a40cc2003741127d0177856b
Henry Rothenberg <hsr205@aol.com>
2025-01-14 09:08:47 -0500

    Add execute logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+import AccountView from "./views/account-view";
+import AccountController from "./controllers/account-controller";
+import ViewInterface from "./interfaces/view-interface";
+import ControllerInterface from "./interfaces/controller-interface";
+import ModelInterface from "./interfaces/model-interface";
+import AccountModel from "./models/account-model";
+
+
+async function runApplication(): Promise<void> {
+
+    const view: ViewInterface = new AccountView();
+    const model: ModelInterface = new AccountModel();
+    const controller: ControllerInterface = new AccountController(view, model);
+
+    controller.startApplication();
+
+}
+
+
+runApplication();
