c6b1b0b0e6afb22634696f6533f190859d2e26dd
Henry Rothenberg <hsr205@aol.com>
2025-01-14 14:42:44 -0500

    Alter method logic

--- src/controllers/account-controller.ts
+++ src/controllers/account-controller.ts
@@ -25,15 +25,14 @@ class AccountController implements ControllerInterface {
             case "1":
                 this._displayAccountRegistrationInstructions()
                 const id: string = await this.view.getUserResponse("Enter Account ID: ");
-                const firstName: string = await this.view.getUserResponse("Enter First Name: ");
-                const lastName: string = await this.view.getUserResponse("Enter Last Name: ");
+                const fullName: string = await this.view.getUserResponse("Enter Full Name: ");
                 const pinNumber: string = await this.view.getUserResponse("Enter Pin Number: ");
 
                 const accountObj: Account = {
                     id: id,
-                    firstName: firstName,
-                    lastName: lastName,
+                    fullName: fullName,
                     pinNumber: pinNumber,
+                    accountBalance: 0
                 };
 
                 this.model.addUserAccount(accountObj);
@@ -44,23 +43,36 @@ class AccountController implements ControllerInterface {
                 const loginId: string = await this.view.getUserResponse("Enter Account ID: ");
                 const loginPinNumber: string = await this.view.getUserResponse("Enter Pin Number: ");
 
-                this.model.loginToUserAccount(loginId, loginPinNumber)
-                this.view.displayAccountActionsMenu()
-                const actionNumber: string = await this.view.getUserResponse("Enter Account Action Number: ");
-
-                switch (actionNumber) {
-                    case "1":
-                        this.model.displayAccountBalance()
-                        break;
-                    case "2":
-                        const fundsToDeposit: string = await this.view.getUserResponse("Enter Number to Deposit: $");
-                        this.model.depositFunds(Number(fundsToDeposit));
-                        break;
-                    case "3":
-                        const fundsToWithdraw: string = await this.view.getUserResponse("Enter Number to Withdraw: $");
-                        this.model.withdrawFunds(Number(fundsToWithdraw))
+                const validLogin: boolean = this.model.loginToUserAccount(loginId, loginPinNumber)
+                if (validLogin) {
+                    let isLoggedIn = true;
+                    while (isLoggedIn) {
+                        this.view.displayAccountActionsMenu();
+                        const actionNumber: string = await this.view.getUserResponse("Enter Account Action Number: ");
+                        switch (actionNumber) {
+                            case "1":
+                                this.model.displayAccountBalance(loginId);
+                                break;
+                            case "2":
+                                const fundsToDeposit: string = await this.view.getUserResponse("Enter Amount to Deposit: $");
+                                this.model.depositFunds(loginId, Number(fundsToDeposit));
+                                break;
+                            case "3":
+                                const fundsToWithdraw: string = await this.view.getUserResponse("Enter Amount to Withdraw: $");
+                                this.model.withdrawFunds(loginId, Number(fundsToWithdraw));
+                                break;
+                            case "4":
+                                console.log("\nLogging out of the account...");
+                                isLoggedIn = false;
+                                break;
+                            default:
+                                console.log("\nInvalid action. Please try again.");
+                                break;
+                        }
+                    }
                 }
 
+                this.view.displayCommandMenu();
                 await this.handleUserResponse(await this.view.getUserResponse("Enter Action Number: "));
                 break;
             case "3":
@@ -80,13 +92,12 @@ class AccountController implements ControllerInterface {
     private _displayAccountRegistrationInstructions(): void {
         console.log(`
                 Please enter your account registration information with the following values:
-                (ID, FIRST-NAME, LAST-NAME, PIN-NUMBER)
+                (ID, FULL-NAME, PIN-NUMBER)
                 
                 For Example:
                 
-                Enter First Name: 10001
-                Enter First Name: John
-                Enter Last Name: Smith
+                Enter Account ID: 10001
+                Enter Full Name: John Smith
                 Enter Pin Number: 12345
                 `);
     }
